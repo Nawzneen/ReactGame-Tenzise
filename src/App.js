0f229396944c32890b35cdd08e3bdf455b8ec9f1
Nazanin Nak <earworm90@gmail.com>
2022-10-08 18:10:35 +0300

    Finishing the game

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import "./App.css";
 import Dice from "./Components/Dice";
 import React from "react";
 import { nanoid } from "nanoid";
+import Confetti from "react-confetti";
 
 function App() {
   const [dice, setDice] = React.useState(generateDice());
-  // const [selectedDice, setSelectedDice] = React.useState([]);
+
   const [game, setGame] = React.useState(false);
+  const [clickedNum, setClickedNum] = React.useState(0);
+
   React.useEffect(() => {
     const allHeld = dice.every((die) => die.isHeld);
     const firstDie = dice[0].value;
@@ -20,26 +23,18 @@ function App() {
   function generateDice() {
     const newArray = [];
     for (let i = 0; i < 10; i++) {
-      const randomNumber = Math.floor(Math.random() * 8);
+      const randomNumber = Math.floor(Math.random() * 3);
       newArray.push({ value: randomNumber, isHeld: false, id: nanoid() });
-      // console.log(newArray);
     }
     return newArray;
   }
   function start() {
     setGame(false);
+    setClickedNum(0);
+
     setDice(generateDice());
   }
-  // Hold Each Die
-  // function holdDice(id) {
-  //   // console.log(id);
-  //   setDice((prevDice) =>
-  //     prevDice.map((die) => {
-  //       return die.id === id ? { ...die, isHeld: !die.isHeld } : die;
-  //     })
-  //   );
-  //   console.log(dice);
-  // }
+
   function holdDice(id) {
     setDice((oldDice) =>
       oldDice.map((die) => {
@@ -50,7 +45,6 @@ function App() {
   const diceElement = dice.map((die) => (
     <Dice
       value={die.value}
-      // id={die.id}
       key={die.id}
       isHeld={die.isHeld}
       holdDice={() => holdDice(die.id)}
@@ -59,9 +53,10 @@ function App() {
 
   // Roll the Dice Agiain
   function roll() {
+    setClickedNum((prevClickedNum) => prevClickedNum + 1);
     setDice((prevDice) =>
       prevDice.map((die) => {
-        const randomNumber = Math.floor(Math.random() * 8);
+        const randomNumber = Math.floor(Math.random() * 3);
         return die.isHeld
           ? die
           : { value: randomNumber, isHeld: false, id: nanoid() };
@@ -69,21 +64,24 @@ function App() {
     );
   }
 
-  // const diceArrayElements = diceArray.map((dice, index) => (
-  //   <Dice key={index} value={dice} index={index} selectDice={selectDice} />
-  // ));
-  // Adding selected dices to an array
-  // const [selectedDicesArray, setSelectedDicesArray] = React.useState([]);
-  // function selectDice(event, id) {
-  //   console.log(id);
-  //   setSelectedDicesArray((prevDices) => [...prevDices, diceArray[id]]);
-  //   console.log(selectedDicesArray);
-  // }
-
   return (
     <div className="App">
       <main>
-        <div className="box">{diceElement}</div>
+        <div className="rules">
+          <h1>Tenzise</h1>
+          <p>Roll and select all the same numbers to win</p>
+        </div>
+        {game && <Confetti />}
+        <div className="box">
+          {" "}
+          <div className={game ? "win-board" : "hide"}>
+            <p>
+              Congradulation, you have won with{" "}
+              <span className="">{clickedNum}</span> clickes
+            </p>
+          </div>
+          {diceElement}
+        </div>
       </main>
       <button className={game ? "initiate hide" : "initiate"} onClick={roll}>
         Roll
--- src/Components/Dice.js
+++ src/Components/Dice.js
@@ -1,9 +1,6 @@
 import React, { Component } from "react";
 
 export default function Dice(props) {
-  // const styles = {
-  //   backgroundColor: props.isHeld ? "white" : "blue",
-  // };
   return (
     <div
       className={props.isHeld ? "dice--container isHeld " : "dice--container  "}
